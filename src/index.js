1da5e72dadf4f8b7af69e7a8a8b5b317f102c90e
Mauricio Contreras <virtual.kur2@gmail.com>
2019-08-19 15:54:47 +0200

    db helper connection created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,52 @@
 const server = require('./server');
 const config = require('./config');
+const dbHelper = require('./server/helpers/db.helper');
 
 const app = server();
-app.listen(config.port, () => {
-  console.log(`Server started and listening on port: ${config.port}`);
+console.info('Starting app...');
+//try to connect to db:
+dbHelper.connect()
+  .then(() => {
+    console.info('Starting server...');
+    app.listen(config.port, (err) => {
+      if(err) {
+        console.error(err.message);
+        process.exit(1);
+      }
+      console.log(`Server started and listening on port: ${config.port}`);
+    });
+  })
+  .catch((e) => {
+    console.error(e.message);
+    process.exit(1);
+  });
+
+
+process.on('beforeExit', () => {
+  console.log('Preparing for close app...');
+  dbHelper.disconnect()
+    .then(() => {
+      console.info('...');
+    })
+    .catch((err) => {
+      console.error('Error: ', err);
+    });
+});
+
+process.on('exit', (exitCode) => {
+  console.info('===> A process.exit() was called with code ', exitCode);
+  console.info('...closing app.')
+  console.info('Bye');
+});
+
+process.on('SIGINT', () => {
+  console.info('\n===> <ctrl+c> detected');
+  dbHelper.disconnect()
+    .then(() => {
+      console.info('...');
+    })
+    .catch((err) => {
+      console.error('Error: ', err);
+    });
+  process.exit(1);
 });
