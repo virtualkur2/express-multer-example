ad823adee4327460816778b6c502b91a2690966d
Mauricio Contreras <virtual.kur2@gmail.com>
2019-08-19 15:25:05 +0200

    database connection helper

--- src/server/helpers/db.helper.js
+++ src/server/helpers/db.helper.js
@@ -16,8 +16,47 @@ const options = {
 
 const helper = {
   connect: () => {
+    return mongoose.connect(uri, options);
+  },
+  disconnect: () => {
     return new Promise((resolve, reject) => {
-      
+      db.close()
+        .then(() => resolve())
+        .catch((err) => reject(err));
     });
   }
 }
+
+db.on('connecting', () => {
+  console.info('===> Trying to connect to database...');
+});
+
+db.on('connected', () => {
+  console.info('===> Succesfully connected to database.');
+});
+
+db.on('disconnecting', () => {
+  console.info('===> Disconnecting from database...');
+});
+
+db.on('disconnected', () => {
+  console.info('===> Database connection lost.');
+});
+
+db.on('close', () => {
+  console.info('===> Database connection closed succesfully.');
+});
+
+db.on('fullsetup', () => {
+  console.info('===> Replica set: Succesfully connected to Primary and at least one Secondary');
+});
+
+db.on('all', () => {
+  console.info('===> Replica set: Succesfully connected to All servers of the set.');
+});
+
+db.on('error', (err) => {
+  console.error(`===> Error connecting to database: ${err}`);
+});
+
+module.exports = helper;
