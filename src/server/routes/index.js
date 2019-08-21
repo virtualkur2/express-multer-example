4de45f9af84b192ed809487789218e865be239cc
Mauricio Contreras <virtual.kur2@gmail.com>
2019-08-21 14:58:13 +0200

    added movies route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,9 +1,11 @@
 const auth = require('./auth');
 const users = require('./users');
+const movies = require('./movies');
 
 const routes = (router) => {
   auth(router);
   users(router);
+  movies(router);
   return router;
 }
 
