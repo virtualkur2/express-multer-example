56bd342c0adf67085a12951b482a0520ca97de1e
Mauricio Contreras <virtual.kur2@gmail.com>
2019-08-21 14:58:40 +0200

    movies route created

--- src/server/routes/movies/index.js
+++ src/server/routes/movies/index.js
@@ -1,1 +1,9 @@
 const authCtrl = require('../../controllers/auth.controller');
+const movieCtrl = require('../../controllers/movie.controller');
+
+const movies = (router) => {
+  router.route('/api/movies/list')
+    .get(authCtrl.requireSignin, movieCtrl.list);
+}
+
+module.exports = movies;
