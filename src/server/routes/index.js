3dda15d39a8233c66f67d265d08b4b61364868e9
Mauricio Contreras <virtual.kur2@gmail.com>
2019-08-29 15:10:33 +0200

    genres route added

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,11 +1,13 @@
 const auth = require('./auth');
 const users = require('./users');
 const movies = require('./movies');
+const genres = require('./genres');
 
 const routes = (router) => {
   auth(router);
   users(router);
   movies(router);
+  genres(router);
   return router;
 }
 
