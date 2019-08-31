71bd05f8b94e35b7e816d67d810193ea32331446
Mauricio Contreras <virtual.kur2@gmail.com>
2019-08-31 12:31:04 +0200

    added requireSignIn middleware to movies routes

--- src/server/routes/movies/index.js
+++ src/server/routes/movies/index.js
@@ -3,26 +3,14 @@ const movieCtrl = require('../../controllers/movie.controller');
 
 const movies = (router) => {
   router.route('/api/movie/create')
-    .get((req, res, next) => {
-      const template = `
-      <form action="/api/movies/create" method="post" enctype="multipart/form-data">
-        <label for="title">Title</label><br>
-        <input type="text" name="title"/><br>
-        <label for="image">Image</label><br>
-        <input type="file" name="image" /><br>
-        <button type="submit">Enviar</button>
-      </form>
-      `;
-      return res.status(200).send(template);
-    })
-    .post(movieCtrl.upload.single('image'), movieCtrl.create);
+    .post(authCtrl.requireSignin, authCtrl.hasAuthorization, movieCtrl.upload.single('image'), movieCtrl.create);
 
   router.route('/api/movie/id/:movieId')
-    .put(movieCtrl.upload.single('image'), movieCtrl.update)
-    .get(movieCtrl.read);
+    .put(authCtrl.requireSignin, authCtrl.hasAuthorization, movieCtrl.upload.single('image'), movieCtrl.update)
+    .get(authCtrl.requireSignin, movieCtrl.read);
 
   router.route('/api/movie/title/:movieTitle')
-    .get(movieCtrl.read);
+    .get(authCtrl.requireSignin, movieCtrl.read);
 
   router.route('/api/movie/list')
     .get(movieCtrl.list);
